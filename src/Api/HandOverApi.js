b7dd54d64e92cd0302010a20689a64d5175b059d
eistcstwo <eistcstwo@gmail.com>
2025-10-03 17:42:01 +0530

    Update HandOverApi.js

--- src/Api/HandOverApi.js
+++ src/Api/HandOverApi.js
@@ -1,11 +1,37 @@
+
 import axios from 'axios';
 
 // Create axios instance with custom configuration
 const api = axios.create({
   baseURL: 'https://10.191.171.12:5443/EISHOME_TEST/shiftHandover',
- // baseURL: 'https://10.191.171.12:5443/EISHOME/'
   timeout: 100000,
   headers: {
     'Content-Type': 'application/json',
   }
 });
+
+
+
+export const getHandovers = async () => {
+  try {
+    const uid = localStorage.getItem('uidd');
+    const password = localStorage.getItem('password');
+    
+    const payload = {
+      uid,
+      password,
+    };
+    
+    const response = await api.post('/get_Handover/', payload, {
+      timeout: 30000,
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    });
+    
+    return response;
+  } catch (error) {
+    console.error('Could not fetch handovers:', error);
+    throw error;
+  }
+};
